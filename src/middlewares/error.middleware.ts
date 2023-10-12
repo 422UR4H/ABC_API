817cb031cf85929f55b5b4279ffc5cf2978fa7f2
422UR4H <tiagosindra@id.uff.br>
2023-10-12 16:43:51 -0300

    :sparkles: feat: errorMiddleware

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+
+export default function errorHandler(error: any, _req: Request, res: Response, _next: NextFunction) {
+    if (error.type === "conflict") {
+        return res.status(httpStatus.CONFLICT).send(error.message);
+    }
+    if (error.type === "badRequest") {
+        return res.status(httpStatus.BAD_REQUEST).send(error.message);
+    }
+    if (error.type === "notFound") {
+        return res.status(httpStatus.NOT_FOUND).send(error.message);
+    }
+
+    if (error.type === "incompleteData") {
+        return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(error.message);
+    }
+
+    if (error.type === "invalidId") {
+        return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(error.message)
+    }
+    if (error.type === "internal") {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message)
+    }
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).send(`Sorry, something went ${error}`);
+}
