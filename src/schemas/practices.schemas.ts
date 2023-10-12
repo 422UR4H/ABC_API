ca04905dd7a435ef3190acd1e79ea9c58181aef7
422UR4H <tiagosindra@id.uff.br>
2023-10-12 19:57:50 -0300

    :sparkles: feat: practices

---
+++ src/schemas/practices.schemas.ts
@@ -0,0 +1,19 @@
+import Joi from "joi";
+import {
+    PracticeCreateInput,
+    PracticeUpsert,
+} from "@/protocols/practices.protocols";
+import { PracticeParams } from "@/protocols/practices.protocols";
+
+export const practiceBody = Joi.object<PracticeCreateInput>({
+    name: Joi.string().required(),
+});
+
+export const practiceParams = Joi.object<PracticeParams>({
+    practiceId: Joi.number().required(),
+});
+
+export const practiceAdvantageBody = Joi.object<PracticeUpsert>({
+    advantage: Joi.string().required(),
+    description: Joi.string().required(),
+});
---
+++ src/services/practices.service.ts
@@ -0,0 +1,37 @@
+import { practiceRepository } from "@/repositories/practices.repository";
+
+export async function createPractice(name: string) {
+    return await practiceRepository.createPractice({ name });
+}
+
+export async function getPractice() {
+    return await practiceRepository.getPractice();
+}
+
+export async function updatePractice(id: number, name: string) {
+    return await practiceRepository.updatePractice({ id, name });
+}
+
+export async function upsertPracticeAdvantage(
+    advantage: string,
+    description: string,
+    practiceId: number
+) {
+    return await practiceRepository.upsertPracticeAdvantage(
+        advantage,
+        description,
+        practiceId
+    );
+}
+
+export async function deletePractice(practiceId: number) {
+    return await practiceRepository.deletePractice(practiceId);
+}
+
+export const practiceServices = {
+    createPractice,
+    getPractice,
+    updatePractice,
+    upsertPracticeAdvantage,
+    deletePractice,
+};
