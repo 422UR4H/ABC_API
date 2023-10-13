12be2e2415efe48eebef54b388dac2f25a9df6e9
422UR4H <tiagosindra@id.uff.br>
2023-10-13 02:53:38 -0300

    :sparkles: feat: getForum

--- src/controllers/forum.controller.ts
+++ src/controllers/forum.controller.ts
@@ -1,9 +1,10 @@
 import httpStatus from 'http-status';
 import { Request, Response } from 'express';
-import { forumServices } from '@/services/forum.service';
+import { forumService } from '@/services/forum.service';
+import { ForumParams } from '@/protocols';
 
-export async function getForum(_req: Request, res: Response) {
-    const { category, }
-    const result = await forumServices.getForum();
+export async function getForum(req: Request, res: Response) {
+    const { category } = req.params as ForumParams;
+    const result = await forumService.findForum(category);
     return res.status(httpStatus.OK).send(result);
 }
--- src/routers/forum.routes.ts
+++ src/routers/forum.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { validateParams } from "@/middlewares/schema.middleware";
+import { forumParams } from "@/schemas/forum.schemas";
+import { getForum } from "@/controllers/forum.controller";
 
-const forumRouter = Router();
+export const forumRouter = Router();
 
 forumRouter
-  .get("/", validateParams(ForumP), getForum)
-
-export default forumRouter;
+  .get("/", validateParams(forumParams), getForum)
